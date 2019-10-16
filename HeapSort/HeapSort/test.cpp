130f9c744f9803a8c476ec3da3b7caa4321db7d3
Leo <964952155@qq.com>
2019-10-16 19:03:27 +0800

    堆排序模拟实现

---
+++ HeapSort/HeapSort/test.cpp
@@ -0,0 +1,69 @@
+
+//¶ÑÅÅÐò
+#include<iostream>
+using namespace std;
+
+//½»»»
+void Swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+//½¨¶Ñ
+void Heap_build(int array[], int root, int length)
+{
+	int lchild = root * 2 + 1;
+	if (lchild < length)
+	{
+		int flag = lchild;
+		int rchild = lchild + 1;
+		if (rchild < length)
+		{
+			if (array[rchild] > array[flag])
+			{
+				flag = rchild;
+			}
+		}
+		if (array[root] < array[flag])
+		{
+			Swap(array[root], array[flag]);
+			Heap_build(array, flag, length);
+		}
+	}
+}
+
+//¶ÑÅÅÐò
+void HeapSort(int array[], int len)
+{
+	for (int i = len / 2; i >= 0; i--)
+	{
+		Heap_build(array, i, len);
+	}
+
+	for (int j = len - 1; j > 0; --j)
+	{
+		Swap(array[0], array[j]);
+		Heap_build(array, 0, j);
+	}
+
+}
+
+
+int main()
+{
+	int array[] = { 1, 34, 34, 0, 121, 1, -1, -5, 13123, 0 };
+	int size = sizeof(array) / sizeof(array[0]);
+
+	HeapSort(array, size);
+
+	//·¶Î§forÑ­»·±éÀú´òÓ¡Êý×é
+	for (auto e : array)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
